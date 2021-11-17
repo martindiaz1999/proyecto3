2e3f089953d5c12d15d49e415b4246987953293c
martindiaz1999 <martindiaz235@gmail.com>
2021-11-17 18:31:56 -0300

    new

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+/*
+let martin = 1999
+let variable1 = 49
+let variable2 = "River plate"
+let variable3 = martin
+console.log(variable3)
+console.log(variable2)
+console.log(variable1)
+martin = 2018
+console.log(martin)
+let equipos = {
+   "nombre": "river",              array
+   "estadio": "monumental",
+   "fundación": 1929,
+   "capacidad": 45000,
+   "valor" : 55000
+
+}
+console.log(equipos.estadio)        
+console.log(equipos.fundación)
+console.log(equipos.capacidad + equipos.valor)
+console.log(`el valor definitivo es ${equipos.capacidad+equipos.valor}`)
+
+let marcas = ["river","boca","racing"]
+console.log=(marcas[1])                  arreglo
+
+
+let river = 2010
+
+let river = true
+if(river == true){console.log(river)  booleano
+}else{console.log("error")}
+*/
+
 $(document).ready(()=>{
 
     $("#botonCalzado").click(()=>{
